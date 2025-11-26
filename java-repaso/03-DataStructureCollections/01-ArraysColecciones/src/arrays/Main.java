f4701afe624aaf565712cf5093f90f1cbd03311e
DanieliniCaporal <dasacaporal@gmail.com>
2025-11-25 23:53:04 -0600

    Adding example document

--- java-repaso/03-DataStructureCollections/01-ArraysColecciones/src/arrays/Main.java
+++ java-repaso/03-DataStructureCollections/01-ArraysColecciones/src/arrays/Main.java
@@ -1,5 +1,8 @@
 package arrays;
 
 public class Main {
-    
+    public static void main(String[] args) {
+        Integer[] numeros = {1,2,3,4,5};
+        System.out.println(numeros[1]);
+    }
 }
