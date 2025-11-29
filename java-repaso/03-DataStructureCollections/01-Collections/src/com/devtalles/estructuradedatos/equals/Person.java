f8a11a2ac31a1ca94bb23aaf6b8dac71650b396d
DanieliniCaporal <dasacaporal@gmail.com>
2025-11-28 23:51:38 -0600

    feat: how works equals

--- java-repaso/03-DataStructureCollections/01-Collections/src/com/devtalles/estructuradedatos/equals/Person.java
+++ java-repaso/03-DataStructureCollections/01-Collections/src/com/devtalles/estructuradedatos/equals/Person.java
@@ -1,5 +1,7 @@
 package com.devtalles.estructuradedatos.equals;
 
+import java.util.Objects;
+
 public class Person {
     private String name;
 
@@ -12,6 +14,16 @@ public class Person {
         if(this == obj) return true;
         if(obj == null || getClass() != obj.getClass()) return false;
         Person person = (Person)obj;
+        return Objects.equals(name, person.name);
 //        return super.equals(obj);
+    }
+
+    public static void main(String[] args) {
+        Person persona1 = new Person("Daniel");
+        Person persona2 = new Person("Daniel");
+        Person persona3 = new Person("Nancy");
+
+        System.out.println(persona2.equals(persona1));
+        System.out.println(persona2.equals(persona3));
     }
 }
