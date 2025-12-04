7035fcaaf7ab5e8875356bea621c0e2d2bbf63e3
DanieliniCaporal <dasacaporal@gmail.com>
2025-12-03 23:53:41 -0600

    Example exception

--- java-repaso/04-ExceptionHandling/01-Exceptions/01-ExceptionsJava/src/introducction/Main.java
+++ java-repaso/04-ExceptionHandling/01-Exceptions/01-ExceptionsJava/src/introducction/Main.java
@@ -1,7 +1,30 @@
 package introducction;
 
+class InvalidAgeException extends RuntimeException {
+    public InvalidAgeException(String message) {
+        super(message);
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
 
+        int age = -5;
+
+        try {
+            validateAge(age);
+            System.out.println("Edad válida. Continuando con el programa...");
+        } catch (InvalidAgeException e) {
+            System.err.println("Error: " + e.getMessage());
+        }
+
+        System.out.println("Programa finalizado correctamente.");
+    }
+
+    // Método que valida y lanza la excepción
+    private static void validateAge(int age) {
+        if (age < 0) {
+            throw new InvalidAgeException("La edad no puede ser negativa: " + age);
+        }
     }
 }
