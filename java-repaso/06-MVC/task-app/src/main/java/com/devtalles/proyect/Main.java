34dd6f9288a02842bc4643082e7af23f3e4cb347
DanieliniCaporal <dasacaporal@gmail.com>
2026-01-13 23:56:22 -0600

    feat(view): Finish view, adding menu and CRUD

--- java-repaso/06-MVC/task-app/src/main/java/com/devtalles/proyect/Main.java
+++ java-repaso/06-MVC/task-app/src/main/java/com/devtalles/proyect/Main.java
@@ -1,17 +1,17 @@
 package com.devtalles.proyect;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import com.devtalles.proyect.task.controller.TaskController;
+import com.devtalles.proyect.task.model.TaskRepository;
+import com.devtalles.proyect.task.view.TaskView;
+
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        TaskRepository repository = new TaskRepository();
+        TaskController controller = new TaskController(repository);
+        TaskView view = new TaskView(controller);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        view.showMenu();
+
     }
 }
--- java-repaso/06-MVC/task-app/src/main/java/com/devtalles/proyect/task/view/TaskView.java
+++ java-repaso/06-MVC/task-app/src/main/java/com/devtalles/proyect/task/view/TaskView.java
@@ -3,6 +3,7 @@ package com.devtalles.proyect.task.view;
 import com.devtalles.proyect.task.controller.TaskController;
 import com.devtalles.proyect.task.exceptions.TaskException;
 import com.devtalles.proyect.task.exceptions.TaskValidationException;
+import com.devtalles.proyect.task.model.Task;
 
 import java.util.Scanner;
 
@@ -11,7 +12,7 @@ public class TaskView {
     private final Scanner scanner;
 
 
-    public TaskView(TaskController taskController, Scanner scanner) {
+    public TaskView(TaskController taskController) {
         this.taskController = taskController;
         this.scanner = new Scanner(System.in);
     }
@@ -32,37 +33,28 @@ public class TaskView {
                     addTaskView();
                     break;
                 case "2":
-
+                    removeTaskView();
                     break;
                 case "3":
-
+                    updateTaskView();
                     break;
                 case "4":
-
+                    showTaskView();
                     break;
                 case "5":
-
-                    break;
+                    System.out.println("Exit...");
+                    return;
+                    default:
+                        System.out.println("Invalid option, try again");
             }
 
         }
     }
 
     public void addTaskView(){
-
         try{
-            System.out.println("Enter an id: ");
-            String id = scanner.nextLine();
-
-            System.out.println("Enter title:");
-            String title = scanner.nextLine();
-
-            System.out.println("Enter description:");
-            String description = scanner.nextLine();
-
-            System.out.println("Is the task complete? true/false");
-            Boolean completed = Boolean.parseBoolean(scanner.nextLine());
-            taskController.addTask(id, title, description, completed);
+            Task task = getTaskInput();
+            taskController.addTask(task.getId(), task.getTitle(), task.getDescription(), task.getCompleted());
             System.out.println("Task has been added successfully!");
         } catch (TaskValidationException | TaskException e){
             System.out.println("Error: " + e.getMessage());
@@ -84,7 +76,53 @@ public class TaskView {
             System.out.println("Error: " + e.getMessage());
             throw new RuntimeException(e);
         }
-
-
     }
+
+    public void showTaskView(){
+        try {
+            System.out.println("\nTask Management System: ");
+            this.taskController.showsTasks();
+        } catch (TaskValidationException | TaskException e) {
+            System.out.println("Error: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void updateTaskView() {
+        try{
+            Task task = getTaskInput();
+            taskController.updateTask(task.getId(), task.getTitle(), task.getDescription(), task.getCompleted());
+            System.out.println("Task has been update successfully!");
+        } catch (TaskValidationException | TaskException e){
+            System.out.println("Error: " + e.getMessage());
+        } catch (Exception e){
+            System.out.println("Error: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    private Task getTaskInput(){
+        String id;
+        do {
+            System.out.println("Enter an id: ");
+            id = scanner.nextLine();
+            if(id.isEmpty()){
+                System.out.println("Invalid id... is empty!");
+            }
+        } while (id.isEmpty());
+
+        System.out.println("Enter title:");
+        String title = scanner.nextLine();
+
+        System.out.println("Enter description:");
+        String description = scanner.nextLine();
+
+        System.out.println("Is the task complete? true/false");
+        Boolean completed = Boolean.parseBoolean(scanner.nextLine());
+
+        return new Task(id, title, description, completed);
+    }
+
 }
